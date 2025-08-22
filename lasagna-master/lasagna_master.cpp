a633b0b8bd5fb6941ac8ec145ea4c2abedf8f9f5
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2025-08-22 14:23:20 -0600

    feat: complete lasagna-master exercise

--- lasagna-master/lasagna_master.cpp
+++ lasagna-master/lasagna_master.cpp
@@ -2,6 +2,26 @@
 
 namespace lasagna_master {
 
-// TODO: add your solution here
+int preparationTime(std::vector<std::string> layers, int layer_prep_time) { return layers.size() * layer_prep_time; }
+amount quantities(std::vector<std::string> layers) {
+    amount amount_info;
+    amount_info.noodles = 0;
+    amount_info.sauce = 0;
+    for (auto layer : layers) {
+        if (layer == "noodles") { amount_info.noodles += 50; }
+        if (layer == "sauce") { amount_info.sauce += 0.2; }
+    }
+    return amount_info;
+}
+void addSecretIngredient(std::vector<std::string> &my_ingredients, const std::vector<std::string> &friends_ingredients) { my_ingredients.back() = friends_ingredients.back(); }
+void addSecretIngredient(std::vector<std::string> &my_ingredients, const std::string &secret_ingredient) { my_ingredients.back() = secret_ingredient; }
+std::vector<double> scaleRecipe(const std::vector<double> &quantities, int new_portions) {
+    std::vector<double> new_quantities;
+    for (auto q : quantities) {
+        double one_portion = q / 2;
+        new_quantities.push_back(one_portion * new_portions);
+    }
+    return new_quantities;
+}
 
 }  // namespace lasagna_master
--- lasagna-master/lasagna_master.h
+++ lasagna-master/lasagna_master.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace lasagna_master {
 
@@ -6,5 +8,11 @@ struct amount {
     int noodles;
     double sauce;
 };
+
+int preparationTime(std::vector<std::string> layers, int layer_prep_time = 2);
+amount quantities(std::vector<std::string> layers);
+void addSecretIngredient(std::vector<std::string> &my_ingredients, const std::vector<std::string> &friends_ingredients);
+void addSecretIngredient(std::vector<std::string> &my_ingredients, const std::string &secret_ingredient);
+std::vector<double> scaleRecipe(const std::vector<double> &quantities, int new_portions);
 
 }  // namespace lasagna_master
