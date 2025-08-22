f6f67a77e45c62dddc9534083d96f3f2342ab9e0
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2025-08-22 16:25:46 -0600

    feat: complete speedywagon exercise

--- speedywagon/speedywagon.cpp
+++ speedywagon/speedywagon.cpp
@@ -18,4 +18,29 @@ int uv_light_heuristic(std::vector<int>* data_array) {
     return uv_index;
 }
 
+bool connection_check(pillar_men_sensor* sensor) {
+    if (sensor != nullptr) return true;
+    return false;
+}
+
+int activity_counter(pillar_men_sensor* sensors, int capacity) {
+    if (sensors == nullptr) return 0;
+    int total_activity = 0;
+    for (int i = 0; i < capacity; ++i) {
+        total_activity += sensors[i].activity;
+    }
+    return total_activity;
+}
+
+bool alarm_control(pillar_men_sensor* sensor) {
+    if (sensor == nullptr) return false;
+    return sensor->activity > 0;
+}
+
+bool uv_alarm(pillar_men_sensor* sensor) {
+    if (sensor == nullptr) return false;
+    int uv_value = uv_light_heuristic(&sensor->data);
+    return uv_value > sensor->activity;
+}
+
 }  // namespace speedywagon
