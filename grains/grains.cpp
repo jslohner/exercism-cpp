d4b7ce39dd415b4d8c0ba592bdb793cf02ea4a86
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-02-23 14:07:09 -1000

    feat: complete grains exercise

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include "grains.h"
 
 namespace grains {
+    std::uint64_t square(int s_num) {
+        return (uint64_t)1<<(s_num-1);
+    }
 
-// TODO: add your solution here
-
+    std::uint64_t total() {
+        std::uint64_t result = 1;
+        for(int i = 2; i <= 64; i++) {
+            result += square(i);
+        }
+        return result;
+    }
 }  // namespace grains
--- grains/grains.h
+++ grains/grains.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <cstdint>
+
 namespace grains {
-
-// TODO: add your solution here
-
+    std::uint64_t square(int s_num);
+    std::uint64_t total();
 }  // namespace grains
