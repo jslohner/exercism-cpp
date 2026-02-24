c5871e43a09fc6d5de2bba1725b5dbbece520577
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-02-23 14:17:04 -1000

    feat: finish raindrops exercise

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -2,6 +2,24 @@
 
 namespace raindrops {
 
-// TODO: add your solution here
+    std::string convert(int input) {
+        std::string result = "";
+
+        if (input % 3 == 0) {
+            result += "Pling";
+        }
+        if (input % 5 == 0) {
+            result += "Plang";
+        }
+        if (input % 7 == 0) {
+            result += "Plong";
+        }
+
+        if (result == "") {
+            return std::to_string(input);
+        }
+
+        return result;
+    }
 
 }  // namespace raindrops
