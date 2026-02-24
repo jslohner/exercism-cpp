af2d5bafa49875b37053474b822898edeb58a6aa
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-02-23 14:39:21 -1000

    feat: complete pangram exercise

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -1,7 +1,22 @@
 #include "pangram.h"
+#include <map>
 
 namespace pangram {
+    bool is_pangram(std::string sentence) {
+        std::map<char, int> dict = {};
+        for (char c  = 'a'; c <= 'z'; ++c) {
+            dict[c] = 0;
+        }
+        for (char c : sentence) {
+            dict[std::tolower(c)]++;
+        }
 
-// TODO: add your solution here
+        for (const auto pair : dict) {
+            if (pair.second == 0) {
+                return false;
+            }
+        }
 
+        return true;
+    }
 }  // namespace pangram
--- pangram/pangram.h
+++ pangram/pangram.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace pangram {
-
-// TODO: add your solution here
-
+    bool is_pangram(std::string sentence);
 }  // namespace pangram
