0033be77cdb507cb3ec9b5106635bfc93d208ef9
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-03-04 17:56:07 -0700

    feat: complete hamming exercise

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,7 +1,17 @@
 #include "hamming.h"
+#include <stdexcept>
 
 namespace hamming {
-
-// TODO: add your solution here
-
+    int compute(std::string a, std::string b) {
+        if (a.length() != b.length()) {
+            throw std::domain_error("String Length Differs");
+        }
+        int distance = 0;
+        for (std::size_t i = 0; i < a.length(); i++) {
+            if (a[i] != b[i]) {
+                distance++;
+            }
+        }
+        return distance;
+    }
 }  // namespace hamming
--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,7 +1,8 @@
 #pragma once
+#include <string>
 
 namespace hamming {
 
-// TODO: add your solution here
+int compute(std::string a, std::string b);
 
 }  // namespace hamming
