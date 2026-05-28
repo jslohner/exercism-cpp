10386e5779a8d5d6e40f4aaa30bbd59717005a08
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-05-28 14:19:49 -0600

    feat: complete queen-attack exercise

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -2,6 +2,16 @@
 
 namespace queen_attack {
 
-// TODO: add your solution here
+    std::pair<int, int> chess_board::white() const { return white_; }
+    std::pair<int, int> chess_board::black() const { return black_; }
+    bool chess_board::can_attack() const {
+        if (
+            white_.first == black_.first ||
+            white_.second == black_.second ||
+            white_.first - white_.second == black_.first - black_.second ||
+            7 - white_.first - white_.second == 7 - black_.first - black_.second
+        ) return true;
+        return false;
+    }
 
 }  // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,7 +1,23 @@
 #pragma once
+#include <iostream>
 
 namespace queen_attack {
 
-// TODO: add your solution here
+    class chess_board {
+        private:
+            std::pair<int, int> white_;
+            std::pair<int, int> black_;
+        public:
+            explicit chess_board(std::pair<int, int> white, std::pair<int, int> black) : white_(white), black_(black) {
+                if (white.first < 0 || white.first > 7) throw std::domain_error("Queen must have row which exists on the board (integer from 0-7).");
+                if (white.second < 0 || white.second > 7) throw std::domain_error("Queen must have column which exists on the board (integer from 0-7).");
+                if (white.first == black.first && white.second == black.second) throw std::domain_error("Both queen positions must be distinct.");
+                white_ = white;
+                black_ = black;
+            }
+            std::pair<int, int> white() const;
+            std::pair<int, int> black() const;
+            bool can_attack() const;
+    };
 
 }  // namespace queen_attack
