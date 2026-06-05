fb89e21e208215fa3a91f35f9053265391231159
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-06-05 11:35:48 -0600

    fix: simplify solution for allergies exercise

--- allergies/allergies.cpp
+++ allergies/allergies.cpp
@@ -1,6 +1,23 @@
 #include "allergies.h"
 
 namespace allergies {
-    bool allergy_test::is_allergic_to(std::string allergy) { return this->allergies.find(allergy) != this->allergies.end(); }
-    std::unordered_set<std::string> allergy_test::get_allergies() { return this->allergies; }
+    static const std::array<std::string, 8> allergy_list = {
+        "eggs",
+        "peanuts",
+        "shellfish",
+        "strawberries",
+        "tomatoes",
+        "chocolate",
+        "pollen",
+        "cats"
+    };
+    allergy_test::allergy_test(int n) {
+        for (std::size_t i = 0; i < allergy_list.size(); ++i) {
+            if (n & (1 << i)) {
+                this->allergies.emplace(allergy_list[i]);
+            }
+        }
+    }
+    bool allergy_test::is_allergic_to(const std::string& allergy) const { return this->allergies.find(allergy) != this->allergies.end(); }
+    const std::unordered_set<std::string>& allergy_test::get_allergies() const { return this->allergies; }
 }  // namespace allergies
--- allergies/allergies.h
+++ allergies/allergies.h
@@ -3,37 +3,15 @@
 #include <map>
 #include <string>
 #include <unordered_set>
+#include <array>
 
 namespace allergies {
     class allergy_test {
         private:
-            std::map<unsigned int, std::string> a_types = {
-                {1, "eggs"},
-                {2, "peanuts"},
-                {4, "shellfish"},
-                {8, "strawberries"},
-                {16, "tomatoes"},
-                {32, "chocolate"},
-                {64, "pollen"},
-                {128, "cats"},
-            };
             std::unordered_set<std::string> allergies;
         public:
-            allergy_test(int n) {
-                unsigned int skip = 256;
-                while (n > 255) {
-                    n -= skip;
-                    skip *= 2;
-                }
-                for (auto it = a_types.rbegin(); it != a_types.rend(); ++it) {
-                    int diff = n - it->first;
-                    if (diff > -1) {
-                        allergies.emplace(it->second);
-                        n -= it->first;
-                    }
-                }
-            }
-            bool is_allergic_to(std::string allergy);
-            std::unordered_set<std::string> get_allergies();
+            allergy_test(int n);
+            bool is_allergic_to(const std::string& allergy) const;
+            const std::unordered_set<std::string>& get_allergies() const;
     };
 }  // namespace allergies
