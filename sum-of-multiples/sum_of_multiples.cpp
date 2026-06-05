4a6c43a4650e30060564684091c83ff4a18b44c0
Justin Lohner <jslohner@Justins-MacBook-Pro.local>
2026-06-05 12:56:37 -0600

    feat: complete sum_of_multiples exercise

--- sum-of-multiples/sum_of_multiples.cpp
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -1,7 +1,13 @@
 #include "sum_of_multiples.h"
 
 namespace sum_of_multiples {
-
-// TODO: add your solution here
-
+    long to(const std::vector<int>& items, int level) {
+        std::unordered_set<int> multiples;
+        for (const int& base : items) {
+            for (int m = base; m < level; m += base) {
+                multiples.insert(m);
+            }
+        }
+        return std::accumulate(multiples.begin(), multiples.end(), 0);
+    }
 }  // namespace sum_of_multiples
--- sum-of-multiples/sum_of_multiples.h
+++ sum-of-multiples/sum_of_multiples.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <iostream>
+#include <numeric>
+#include <unordered_set>
+#include <vector>
 
 namespace sum_of_multiples {
-
-// TODO: add your solution here
-
+    long to(const std::vector<int>& items, int level);
 }  // namespace sum_of_multiples
